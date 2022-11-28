47d593ddc963a7cf0be51bcf1d093ee9b465c631
Intrstng <Intrstng@gmail.com>
2022-11-28 20:56:07 +0300

    init: task completed

--- src/index.js
+++ src/index.js
@@ -38,7 +38,79 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+let separatedInputArr = [];
+
+for (let i = 0; i < expr.length; i = i + 10) {
+  while (expr.length != 0) {
+    separatedInputArr.push(expr.slice(i, 10));
+    expr = expr.replace(expr.slice(0, 10), '');
+  };
+};
+
+const digitsArr = separatedInputArr.reduce((acc, element) => {
+let sum = '';
+
+for (let i = 0; i < element.length; i++) {
+  sum += element[i];
+};
+
+if (isFinite(sum)) {
+  acc.push(parseInt(sum));
+} else {
+        acc.push(sum);
+};
+return acc;
+}, []);
+
+let separatedDigitsArr = digitsArr.reduce((acc, element) => {
+let elementToString = element.toString();
+if (elementToString.length > 2) {
+  while(elementToString.length != 0) {
+    acc.push(elementToString.substring(0, 2));
+    elementToString = elementToString.slice(2);
+}
+  acc.push('SBL'); //spaceBetweenLetters
+} else {
+    acc.push(elementToString);
+    acc.push('SBL'); //spaceBetweenLetters
+}
+  return acc;
+}, []);
+
+let dotsDashesArr = separatedDigitsArr.reduce((acc, element) => {
+switch (element) {
+  case ('10'): acc.push('.');
+  break;
+  case ('11'): acc.push('-');
+  break;
+  case ('SBL'): acc.push(' ');
+  break;
+  case ('**'): acc.push('**');
+  break;
+};
+/*
+    if (element === '10') acc.push('.');
+    else if (element === '11') acc.push('-');
+    else if (element === 'SBL') acc.push(' ');
+    else if (element === '**') acc.push('**');
+*/
+  return acc;
+}, []);
+
+let dotsDashesStr = dotsDashesArr.join('').split(' ');
+let outputInArr = dotsDashesStr.reduce((acc, element) => {
+  if (element === '**********') {
+  acc.push(' ');
+} else {
+        for (let key in MORSE_TABLE) {
+          if (element === key) {
+            acc.push(MORSE_TABLE[key]);
+    };
+  };
+};
+  return acc;
+}, []);
+  return outputInArr.join('');
 }
 
 module.exports = {
